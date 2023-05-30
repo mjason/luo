48870b91b6728b0187fe18407698317e85ed9a88
MJ <tywf91@gmail.com>
2023-05-30 21:15:42 +0800

    增加中间件模式

--- bin/test.rb
+++ bin/test.rb
@@ -2,9 +2,12 @@
 require "bundler/setup"
 require "luo"
 
-cain = Luo::LLMFunc.cain(middlewares: [Luo::Middleware::Log]) do |c|
-  c.prompt = Luo::PromptTemplate.new(text: "你是一个ruby专家，和我一起结对编程\n我希望你能教我<%= skill %>")
-  c.adapter = Luo::Xinghuo.llm_func_adapter
-end
+# cain = Luo::LLMFunc.cain(middlewares: [Luo::Middleware::Log]) do |c|
+#   c.prompt = Luo::PromptTemplate.new(text: "你是一个ruby专家，和我一起结对编程\n我希望你能教我<%= skill %>")
+#   c.adapter = Luo::Xinghuo.llm_func_adapter
+# end
+#
+# cain.call(skill: "proc怎么做")
 
-cain.call(skill: "proc怎么做")
+env = Luo::Middleware::Env.new name: 'manjia'
+binding.irb
